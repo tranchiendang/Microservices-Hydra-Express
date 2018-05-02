fdccf211a40bd714f8131dd377896bb5055745a7
Chien <tranchiendang@gmail.com>
2018-05-02 17:09:18 +0700

    [ADD] bring docker

---
+++ Vagrantfile
@@ -0,0 +1,7 @@
+ENV["LC_ALL"] = "en_US.UTF-8"
+Vagrant.configure("2") do |config|
+  config.vm.box = "minimum/ubuntu-trusty64-docker"
+  config.vm.hostname = "ubuntu-docker"
+  config.vm.network "private_network", type: "dhcp"
+  config.vm.network "forwarded_port", guest: 5353, host: 6363, protocol: "tcp"
+end
---
+++ invoice/properties.js
@@ -0,0 +1,35 @@
+/*
+const localIP = require('local-ip');
+const default_iface = 'eth0'
+const destination_iface = 'eth1';
+let local_ip = "";
+
+try {
+  local_ip = localIP(destination_iface);
+} catch (err) {
+  // exception, fallback to eth0 interface
+  local_ip = localIP(default_iface);
+}
+*/
+
+let result = {
+  cluster: false,
+  maxSockets: 500,
+  environment: 'development',
+  logPath: '',
+  logRequestHeader: true,
+  logOutboundRequest: true,
+  hydra: {
+    serviceName: 'invoice-service',
+    serviceDescription: 'Invoice Service',
+    //serviceIP: local_ip,
+    serviceIP: "",
+    servicePort: 3000,
+    serviceType: 'invoice',
+    redis: {
+      "url": process.env.REDIS_PORT_6379_TCP_ADDR
+    }
+  }
+}
+
+exports.value = result;
