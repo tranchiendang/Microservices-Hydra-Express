b3ca6de555061b3da3731e7a8412a2b1c8db8c8f
Chien <tranchiendang@gmail.com>
2018-05-04 14:05:58 +0700

    [ADD] integrate hydra and hystrix

--- invoice/index.js
+++ invoice/index.js
@@ -1,17 +1,63 @@
 const config = require('./properties').value;
 const version = require('./package.json').version;
 const hydraExpress = require('hydra-express');
+const Hystrix = require("hystrixjs");
+const Hoek = require('hoek');
 
 const routes = require('./routes');
+const commandFactory = Hystrix.commandFactory;
+
+function defaultRunCommand(command, req, res, next) {
+    return new Promise((resolve, reject) => {
+        const handleResponseStatus = Hoek.once(function handleResponseStatus() {
+            if (config.commandStatusResolver) {
+                // hook to custom command status resolver
+                return config.commandStatusResolver(req, res)
+                .then(resolve)
+                .catch(reject);
+            }
+            resolve();
+        });
+
+        res.once('finish', handleResponseStatus);
+        res.once('close', handleResponseStatus);
+        next();
+    });
+}
 
 function registerRoutesCallback() {
   hydraExpress.registerRoutes({'/v1/invoice': routes});
+  hydraExpress.registerRoutes({'/v1/hystrix_inv': routes});
 }
 
 function registerMiddlewareCallback() {
   let app = hydraExpress.getExpressApp();
-  app.use((req, res, next) => {
-    console.log('req.headers', req.headers);
+
+  app.use('/v1/invoice', (req, res, next) => {
+    let commandPath = req.originalUrl;
+    if (req.method === "GET"){
+      let indexOfSplash = commandPath.lastIndexOf("/");
+      if (indexOfSplash > 0) {
+        commandPath = commandPath.substring(0, indexOfSplash);
+      }
+    }
+
+    const commandBuilder = commandFactory.getOrCreate(commandPath).run(defaultRunCommand);
+    commandBuilder
+          .circuitBreakerErrorThresholdPercentage(50)
+          .timeout(20)
+          .circuitBreakerRequestVolumeThreshold(20)
+          .circuitBreakerSleepWindowInMilliseconds(5000)
+          .statisticalWindowLength(10000)
+          .statisticalWindowNumberOfBuckets(10)
+          .build()
+          .execute(commandPath, req, res, next)
+          .catch(err => {
+              console.log(err);
+          });
+  });
+
+  app.use('/v1/hystrix_inv', (req, res, next) => {
     next();
   });
 }
--- payment/routes/index.js
+++ payment/routes/index.js
@@ -1,6 +1,7 @@
 const hydraExpress = require('hydra-express');
 const express = hydraExpress.getExpress();
 const router = express.Router();
+const hystrixSSEStream = require('hystrixjs').hystrixSSEStream;
 
 const HTTP_OK = 200;
 
@@ -20,13 +21,36 @@ router.post('/create', function(req, res){
 * @description Get payment info
 * @param {function} route handler
 */
-router.get('/:id', function(req, res){
+router.get('/get/:id', function(req, res){
   let id = req.params.id || 0;
+
   hydraExpress.sendResponse(HTTP_OK, res, {
     result : {
       message: "Get payment with id= " + id
     }
   });
+});
+
+/**
+* @description Get hystrix data
+* @param {function} route handler
+*/
+router.get('/hystrix.stream', function(req, res) {
+    res.append('Content-Type', 'text/event-stream;charset=UTF-8');
+    res.append('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+    res.append('Pragma', 'no-cache');
+    res.append('Connection', 'keep-alive');
+    return hystrixSSEStream.toObservable().subscribe(
+        function onNext(sseData) {
+            res.write('data: ' + sseData + '\n\n');
+        },
+        function onError(error) {
+            console.log(error);
+        },
+        function onComplete() {
+            return res.end();
+        }
+    );
 });
 
 module.exports = router;
