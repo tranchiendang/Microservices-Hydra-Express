777af8b5df9bea5109a27f637f26eff85ec42719
Chien <tranchiendang@gmail.com>
2018-05-09 13:10:34 +0700

    [EDIT] test brakes for circuit breaker

--- sale-order/index.js
+++ sale-order/index.js
@@ -6,6 +6,48 @@ const config = require('./hydra_properties').value;
 
 const factory = new HydraServiceFactory(config);
 
+const Brakes = require("brakes");
+
+function promiseCall(req, res, next){
+    return new Promise((resolve, reject) =>{
+      if (res.statusCode == 200) {
+          resolve(res);
+      }
+      else {
+        reject();
+      }
+      next();
+    });
+  };
+
+function fallbackCall(){
+    return new Promise((resolve, reject) =>{
+      resolve('Service not available!');
+    });
+  };
+
+const brake = new Brakes(promiseCall, {
+    statInterval: 2500,
+    threshold: 0.5,
+    circuitDuration: 15000,
+    timeout: 250
+    }
+);
+
+brake.fallback(fallbackCall)
+
+function applyBrakes(req, res, next){
+  brake.exec()
+      .then((result) =>{
+        console.log(`result: ${result}`);
+      })
+      .catch(err =>{
+        console.error(`error: ${err}`);
+      });
+}
+
 factory.init().then(factory => factory.getService(service => {
+  service.use('/v1/so', applyBrakes);
   service.use('/v1/so', routes);
+
 })).catch(e => console.log(e));
